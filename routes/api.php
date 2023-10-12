2bcec0c52146581b4c696286a6cb55dbbf8ea860
Florenz Cuizon <florenzkeirt@Florenzs-MacBook-Air.local>
2023-10-12 12:47:37 +0800

    Added versioning on Controllers and updated the routing

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // api/v1
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
-    Route::apiResource('customers', CustomerController::class);
-    Route::apiResource('invoices', InvoiceController::class);
+    Route::apiResource('customers', CustomerController::class); // Route for the customer controller
+    Route::apiResource('invoices', InvoiceController::class); // Route for the invoice controller
 });
